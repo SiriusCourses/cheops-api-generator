71c707a216ed79349bcf867d79aa1eae4956b1dd
frogofjuly <golubev.ki@sirius.online>
2021-12-07 19:15:23 +0300

    hylomorphism as a way to generate hsModule' from internal representation

--- src/Data/ConfigGen/Traverse.hs
+++ src/Data/ConfigGen/Traverse.hs
@@ -1,13 +1,20 @@
-{-# LANGUAGE RecordWildCards #-}
-
 module Data.ConfigGen.Traverse where
 
-import Control.Monad.Reader       (ReaderT, withReaderT)
-import Control.Monad.State.Strict (StateT)
-import Data.Aeson.KeyMap          (KeyMap)
-import Data.ConfigGen.Parsing     (ParserState)
-import Data.ConfigGen.TypeRep     (ModuleParts (ModuleParts))
-import GHC.SourceGen              (HsDecl', HsType')
+import Control.Monad.Reader       (ReaderT, asks)
+import Control.Monad.State.Strict (StateT, modify)
+
+import Data.List (intersperse)
+import Data.Set  (Set)
+import Util      (capitalise)
+
+import Data.ConfigGen.TypeRep (ModuleName, ModuleParts (ModuleParts), TypeRep)
+import GHC.SourceGen          (HsDecl', HsModule', HsType')
+
+import qualified Data.Aeson.Key    as K
+import           Data.Aeson.KeyMap (KeyMap)
+import qualified Data.Aeson.KeyMap as KM
+
+import qualified Data.ConfigGen.Traverse.Hylo as Hylo
 
 type ModulePrefix = [String]
 
@@ -17,10 +24,47 @@ data ConstructedType =
         , _constrDecl :: HsDecl'
         }
 
-type GeneratorState = KeyMap (Either ModuleParts ConstructedType)
+newtype GeneratorState =
+    GeneratorState
+        { includes :: KeyMap (Either ModuleParts HsModule')
+        }
 
-type Ctx a = ReaderT ModulePrefix (StateT ParserState (Either String)) a
+type Ctx a = ReaderT ModulePrefix (StateT GeneratorState (Either String)) a
 
-meh :: ModuleParts -> Ctx [HsDecl']
-meh ModuleParts {..} = do
+data NodeF a s
+    = Local a (KeyMap s)
+    | Leaf a
+
+data Payload =
+    Payload
+        { title        :: Maybe String
+        , externalDeps :: Set ModuleName
+        , typeRep      :: TypeRep
+        }
+
+instance Functor (NodeF a) where
+    fmap _ (Leaf x)       = Leaf x
+    fmap fab (Local x km) = Local x $ fmap fab km
+
+breakDown :: Hylo.Coalgebra (NodeF Payload) ModuleParts
+breakDown (ModuleParts _jsTitle _externalDeps _localDeps _declaration)
+    | null _localDeps = Leaf payload
+    | otherwise = Local payload _localDeps
+  where
+    payload = Payload _jsTitle _externalDeps _declaration
+
+buildUp :: Hylo.Algebra (NodeF Payload) (Ctx (KeyMap HsModule'))
+buildUp (Leaf (Payload title externalDeps typeRep)) = do
+    namespace <- asks $ mconcat . intersperse "." . fmap capitalise
+    modify $ buildExternalDeps externalDeps
+    return $ KM.singleton (K.fromString namespace) _
+buildUp (Local payload km) = do
+    built <- sequence $ _update_keys_with_prefix <$> km
+    modify $ buildExternalDeps (externalDeps payload)
     return undefined
+
+buildExternalDeps :: Set ModuleName -> GeneratorState -> GeneratorState
+buildExternalDeps =
+    _check_external_dependencies_and_construct_modules_from_them_if_they_are_not_constucted_already
+
+meh = Hylo.hylo breakDown buildUp
---
+++ src/Data/ConfigGen/Traverse/Hylo.hs
@@ -0,0 +1,20 @@
+module Data.ConfigGen.Traverse.Hylo where
+import Control.Arrow ((>>>))
+
+newtype Term f =
+    In
+        { out :: f (Term f)
+        }
+
+type Algebra f a = f a -> a
+
+type Coalgebra f a = a -> f a
+
+cata :: (Functor f) => Algebra f a -> Term f -> a
+cata f = out >>> fmap (cata f) >>> f
+
+ana :: (Functor f) => Coalgebra f a -> a -> Term f
+ana f = f >>> fmap (ana f) >>> In
+
+hylo :: (Functor f) => Coalgebra f a -> Algebra f b -> a -> b
+hylo f g = ana f >>> cata g
