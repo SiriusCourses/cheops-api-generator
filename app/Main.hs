8e6f748585427a984fc98df2f0f5cf7983fc8884
frogofjuly <golubev.ki@sirius.online>
2021-12-21 03:30:08 +0300

    more debug options

--- app/Main.hs
+++ app/Main.hs
@@ -4,19 +4,13 @@
 module Main where
 
 import qualified Data.Bifunctor
-import           Data.Coerce    (coerce)
 import           Data.Foldable  (Foldable (foldl'), traverse_)
 import           Data.String    (IsString (fromString))
 
-import Control.Monad.Except       (runExcept)
-import Control.Monad.Reader       (ReaderT (runReaderT))
-import Control.Monad.State.Strict (StateT (runStateT))
-
-import Data.Yaml (FromJSON, decodeHelper)
+import Data.Yaml (decodeHelper)
 
 import qualified Data.Aeson.Key    as K
 import qualified Data.Aeson.KeyMap as KM
-import           Data.Aeson.Types  (FromJSON (parseJSON))
 
 import System.Directory     (createDirectoryIfMissing)
 import System.FilePath      (takeDirectory, (</>))
@@ -27,31 +21,18 @@ import GHC.Paths     (libdir)
 import GHC.SourceGen (HsModule', showPpr)
 
 import qualified CLI
-import           Data.ConfigGen.Parsing  (ParserResult (..))
-import           Data.ConfigGen.Traverse (Dep (..), GeneratorState (..),
-                                          modulePartsToModules)
+import           Data.ConfigGen.Parsing 
+import           Data.ConfigGen.Traverse
 
+import Control.Monad                           (when)
 import Data.ConfigGen.Parsing.IncludeInjection (eventsFromFile)
-
+import Data.Either                             (isLeft, isRight, fromRight)
 
 newtype GeneratedModules =
     GeneratedModules
         { unGeneratedModules :: [(String, HsModule')]
         }
     deriving newtype (Semigroup, Monoid)
-
-instance FromJSON GeneratedModules where
-    parseJSON v = do
-        p@(ParserResult mainType deps) <- parseJSON @ParserResult v
-        let q =
-                runExcept $
-                runStateT (runReaderT (modulePartsToModules mainType) []) $
-                GeneratorState . KM.fromList $ Data.Bifunctor.bimap fromString ToBuild <$> deps
-        res <-
-            case q of
-                Left s          -> fail s
-                Right (res', _) -> return res'
-        return . coerce $ Data.Bifunctor.first K.toString <$> KM.toList res
 
 collectFiles :: FilePath -> IO [FilePath]
 collectFiles = find always (extension ==? ".yaml")
@@ -63,24 +44,59 @@ main = do
         case chInput of
             CLI.File s -> return [s]
             CLI.Dir s  -> collectFiles s
-    content <- sequence <$> traverse (\t -> decodeHelper (eventsFromFile t)) files
-    let foldedContent =
-            foldl'
-                (\acc (_, em) ->
-                     case em of
-                         Left _                        -> acc
-                         Right (m :: GeneratedModules) -> acc <> m)
-                (mempty :: GeneratedModules) <$>
-            content
-    case foldedContent of
+    traverse_ putStrLn files
+    content <-
+        sequence <$> traverse (\t -> decodeHelper @ParserResult (eventsFromFile t)) files
+    case content of
         Left pe -> print pe
-        Right xs ->
-            saveModule $
-            GeneratedModules . fmap (Data.Bifunctor.first (chOutput </>)) $
-            unGeneratedModules xs
+        Right x0 -> do
+            res <- extractParsedModules x0 files
+            when chDebug $ print "-- resulting parser output:"
+            when chDebug $ print res
+            when (null res) $
+                fail
+                    ("No files are successfully parsed. Content looks like this: " ++
+                     show content)
+            let ini = snd . head $ res
+            let acc = replaceDashesWithUnderscores . postprocessParserResult . foldl' combineParserResults ini $ tail res
+            when chDebug $ print "-- accumulated parser results"
+            when chDebug $ print acc
+            let b = build acc
+            when chDebug $ print "-- Built Modules:"
+            when chDebug $ print . KM.keys $ fromRight mempty b
+            case b of
+                Left s -> fail s
+                Right km ->
+                    saveModule $
+                    Data.Bifunctor.first ((chOutput </>) . K.toString) <$> KM.toList km
   where
-    saveModule :: GeneratedModules -> IO ()
-    saveModule gm = traverse_ (uncurry saveFile) $ unGeneratedModules gm
+    combineParserResults :: ParserResult -> (FilePath, ParserResult) -> ParserResult
+    combineParserResults (ParserResult mainType deps) (p, ParserResult mainType' deps') =
+        ParserResult mainType' newDeps
+      where
+        newDeps = fromKM . KM.insert (fromString p) mainType $ (toKM deps) <> (toKM deps')
+        toKM x = KM.fromList $ Data.Bifunctor.first fromString <$> x
+        fromKM x = Data.Bifunctor.first K.toString <$> KM.toList x
+    extractParsedModules ::
+           Show a
+        => [([a], Either String ParserResult)]
+        -> [FilePath]
+        -> IO [(FilePath, ParserResult)]
+    extractParsedModules x0 paths = do
+        traverse_
+            (\x -> do
+                 when (not . null . fst $ x) (print . fst $ x)
+                 if isLeft . snd $ x
+                     then print x
+                     else return ())
+            x0
+        let res =
+                map (\(p, Right r) -> (p, r)) .
+                filter (\(_, q) -> isRight q) . zipWith (,) paths . map snd $
+                x0
+        return res
+    saveModule :: [(FilePath, HsModule')] -> IO ()
+    saveModule km = traverse_ (uncurry saveFile) $ km
       where
         saveFile :: String -> HsModule' -> IO ()
         saveFile path md = do
