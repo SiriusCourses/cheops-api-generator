0a741b9dc0025ce61c8b998a22e3cfbe7e14f9a1
frogofjuly <golubev.ki@sirius.online>
2022-01-24 20:25:44 +0300

    test dependencies are in test tagets

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,18 +4,24 @@ import Foreign.C             (CBool (..), CString, newCString)
 import Foreign.Marshal.Alloc (free)
 
 foreign import ccall "unsafe_validate" unsafe_validate :: CString -> CString -> IO CBool
+
+foreign import ccall "unsafe_generate" unsafe_generate :: CString -> IO CString
+
+foreign import ccall "dispose_generated_object" dispose_generated_object :: CString -> IO ()
+
 foreign import ccall "start_python" start_python :: IO ()
+
 foreign import ccall "end_python" end_python :: IO ()
 
 main :: IO ()
 main = do
     start_python
-    left <- newCString "{type: string}"
-    right <- newCString "{}"
-    res <- unsafe_validate left right
+    sch <- newCString "{\"type\": \"string\"}"
+    obj <- unsafe_generate sch
+    -- print =<< peekCString obj
+    res <- unsafe_validate obj sch
     putStrLn ""
     print $ res == 1
-    free right
-    free left
+    dispose_generated_object obj
+    free sch
     end_python
--- x :: LatexRequest
